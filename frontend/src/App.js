eb3da88d28ec4038eeb40489f0d38a4742fab7ee
Zach Saffrin <zach@zachsaffrin.com>
2019-03-22 17:55:29 -0400

    Set up base frontend app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  state = {
+    backendPresent: false,
+    checkingBackend: false,
+  };
+
+  componentDidMount() {
+    this.checkForBackend();
+  }
+
+  async checkForBackend() {
+    this.setState({ checkingBackend: true });
+    return await fetch('http://localhost:4382')
+      .then(async res => {
+        this.setState({
+          checkingBackend: false,
+          backendPresent: await res.ok,
+        });
+      })
+      .catch(err => console.error(err));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>JobFinder</h1>
       </div>
     );
   }
