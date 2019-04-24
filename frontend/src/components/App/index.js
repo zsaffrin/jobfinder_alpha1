a4920628f83bc037485a21e5defb345badc6518a
Zach Saffrin <zach@zachsaffrin.com>
2019-04-24 13:08:23 -0400

    Use new api routes to load jobs

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { Component } from 'react';
 
-const App = () => {
-  return (
-    <div>
-      <h1>jobfinder</h1>
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    jobs: [],
+    loadingJobs: false,
+    sources: {}
+  };
+
+  componentDidMount() {
+    this.loadJobs();
+  }
+
+  async loadJobs() {
+    this.setState({ loadingJobs: true });
+    return await fetch('http://localhost:4382/api/jobs')
+      .then(async res => {
+        const { jobs, sources } = await res.json();
+        this.setState({
+          jobs,
+          sources,
+          loadingJobs: false
+        });
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    const { jobs, loadingJobs, sources } = this.state;
+
+    return (
+      <div>
+        <h1>jobfinder</h1>
+        <div>
+          {loadingJobs
+            ? '⌛'
+            : `Loaded ${jobs.length} jobs from ${
+                Object.keys(sources).length
+              } sources`}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
