facd65a9257768a3ba1ef6f5d6d33268306522ed
Zach Saffrin <zach@zachsaffrin.com>
2019-04-23 22:57:51 -0400

    Rebuild frontend on CRA3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,66 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const groupByKey = (data, key, groupTitle, groupItemsKey) =>
-  data.reduce((acc, entry) => {
-    acc.some(i => i[groupTitle] === entry[key])
-      ? acc.forEach(i => {
-          if (i[groupTitle] === entry[key]) {
-            i[groupItemsKey].push(entry);
-          }
-        })
-      : acc.push({
-          [groupTitle]: entry[key],
-          [groupItemsKey]: [entry],
-        });
-
-    return acc;
-  }, []);
-
-class App extends Component {
-  state = {
-    loadingJobs: false,
-    jobs: [],
-  };
-
-  componentDidMount() {
-    this.loadJobs();
-  }
-
-  async loadJobs() {
-    this.setState({ loadingJobs: true });
-    return await fetch('http://localhost:4382/alljobs')
-      .then(async res => {
-        const jobs = await res.json();
-        const sortedJobs = groupByKey(jobs, 'company', 'company', 'jobs').sort(
-          (a, b) => (a.company.toUpperCase() > b.company.toUpperCase() ? 1 : -1)
-        );
-        this.setState({
-          loadingJobs: false,
-          jobs: sortedJobs,
-        });
-      })
-      .catch(err => console.error(err));
-  }
-
-  render() {
-    const { jobs } = this.state;
-
-    return (
-      <div>
-        <h1>JobFinder</h1>
-        {jobs.map(i => (
-          <div key={i.company}>
-            <b>{i.company}</b>
-            <ul>
-              {i.jobs.map(job => (
-                <li key={job.uniqueId}>{job.title}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
