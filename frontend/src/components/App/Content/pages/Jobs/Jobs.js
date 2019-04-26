465766335d7935b3a03f4a18e2a591f033bcfa14
Zach Saffrin <zach@zachsaffrin.com>
2019-04-26 11:11:16 -0400

    Add UserJobs component, a little layout style

--- frontend/src/components/App/Content/pages/Jobs/Jobs.js
+++ frontend/src/components/App/Content/pages/Jobs/Jobs.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import LiveJobs from './LiveJobs';
+import UserJobs from './UserJobs';
 
 const Jobs = () => {
+  const JobsPage = styled.div(({ theme }) => {
+    const { space } = theme;
+    return `
+      display: grid;
+      grid-gap: ${space.lg};
+    `;
+  });
+
   return (
-    <div>
+    <JobsPage>
       <LiveJobs />
-    </div>
+      <UserJobs />
+    </JobsPage>
   );
 };
 
---
+++ frontend/src/components/App/Content/pages/Jobs/UserJobs.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserJobs = () => {
+  return <div>User Jobs</div>;
+};
+
+export default UserJobs;
